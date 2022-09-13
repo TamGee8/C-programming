abd847ad8b4ac6e1e7727369967a376943f426c3
TamGee8 <tam.jaja@yahoo.com>
2022-09-13 10:04:52 -0700

    9times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * times_table -print tables
+ * times_table -prints tables
  *
  * Return: Always 0.
  */
@@ -25,6 +25,11 @@ void times_table(void)
 				{
 					_putchar (' ');
 					_putchar ((c % 10) + '0');
+				}
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
 				}
 			}
 			else
